a03eb1d206fdac220bb8dd476ec624f08c5524bc
Иван <i.domnishev@autotown55.local>
2025-04-26 15:12:56 +0600

    Добавил докстринг к mask_account_card и get_date

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(client_data: str) -> str:
+    '''Функция маскирует номер карты клиента'''
     client_data_divided = client_data.rsplit(" ", 1)
     if client_data_divided[0].startswith("Счет"):
         return client_data_divided[0] + " " + get_mask_account(client_data_divided[1])
@@ -10,6 +11,7 @@ def mask_account_card(client_data: str) -> str:
 
 
 def get_date(date: str) -> str:
+    '''Функция преобразует дату проведения операции'''
     date = "2024-03-11T02:26:18.671407"
     modif_date = date[:10]
     return modif_date[8:10] + "." + modif_date[5:7] + "." + modif_date[0:4]
