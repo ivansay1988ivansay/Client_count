9f48fab3eb78d952dc0b10642d77c748e7963b80
Иван <i.domnishev@autotown55.local>
2025-05-09 20:06:42 +0600

    готова функция transaction_descriptions

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from data import transactions, operation
 from src.processing import filter_by_state, sort_by_date
 from src.widget import mask_account_card, get_date
-from src.generators import filter_by_currency
-
+from src.generators import filter_by_currency, transaction_descriptions
 
 print(mask_account_card("Visa Gold 5999414228426300"))
 
@@ -13,8 +12,13 @@ print(filter_by_state(operation))
 print(sort_by_date(operation))
 
 usd_transactions = filter_by_currency(transactions, "USD")
+descriptions = transaction_descriptions(transactions)
 
 if __name__ == "__main__":
 
     for _ in range(2):
         print(next(usd_transactions))
+
+
+    for _ in range(5):
+        print(next(descriptions))
--- src/generators.py
+++ src/generators.py
@@ -7,4 +7,13 @@ def filter_by_currency(transactions_list, currency):
             yield 'Транзакция по запросу',  transaction
 
 
-print(next(filter_by_currency(transactions, 'USD')))
+# print(next(filter_by_currency(transactions, 'USD')))
+
+def transaction_descriptions(transactions_list):
+    for transaction in transactions_list:
+        yield transaction["description"]
+
+if __name__ == "__main__":
+    descriptions = transaction_descriptions(transactions)
+    for _ in range(5):
+        print(next(descriptions))
