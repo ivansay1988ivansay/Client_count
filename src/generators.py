ee7a0d020df861870a6868cf435cb0ec43fe881c
Иван <i.domnishev@autotown55.local>
2025-05-09 21:26:03 +0600

    готова функция card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -13,7 +13,24 @@ def transaction_descriptions(transactions_list):
     for transaction in transactions_list:
         yield transaction["description"]
 
+
+def card_number_generator(start, end):
+    counter = start
+    while counter <= end:
+        number_str = str(counter)
+        full_number = '0' * (16 - len(number_str)) + number_str
+        divided_number = ' '.join([full_number[i:i+4] for i in range(0, 16, 4)])
+        yield divided_number
+        counter += 1
+
+
+for card in card_number_generator(1, 100):
+    print(card)
+
+
 if __name__ == "__main__":
     descriptions = transaction_descriptions(transactions)
     for _ in range(5):
         print(next(descriptions))
+
+print(card_number_generator(1, 7))
